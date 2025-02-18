b1e0f804dacdefaa2facfa200f5c37820c52d96d
tempaccount42069 <mateohernandez2@proton.me>
2025-02-17 22:16:47 -0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,22 +1,22 @@
 local gameScripts = {
-    [1167791961] =
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/tempaccount42069/KitkatHub/refs/heads/main/KnifeSimulator.lua",true))(),
-    [17258520007] =
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/tempaccount42069/KitkatHub/refs/heads/main/BelluPiece.lua",true))(),
-    [1122334455] = [[
+    [1167791961] = function() 
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/tempaccount42069/KitkatHub/refs/heads/main/KnifeSimulator.lua", true))()
+    end,
+    [17258520007] = function() 
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/tempaccount42069/KitkatHub/refs/heads/main/BelluPiece.lua", true))()
+    end,
+    [1122334455] = function() 
         print("Script for Game 1122334455 loaded!")
         -- Add your script content here for game ID 1122334455
-    ]],
+    end,
 }
-
 
 local currentGameID = game.PlaceId
 
 if gameScripts[currentGameID] then
-
     local scriptToLoad = gameScripts[currentGameID]
     local success, errorMsg = pcall(function()
-        loadstring(scriptToLoad)()
+        scriptToLoad() -- Call the stored function to execute the script
     end)
     
     if not success then
