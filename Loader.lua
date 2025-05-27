7050f6e031f62cf188ecf64f85a70019d09123f8
tempaccount42069 <mateohernandez2@proton.me>
2025-05-27 15:53:09 -0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,16 +1,12 @@
 local gameScripts = {
 [1167791961] = function()
--- This will only load when the script is called for this game ID
 loadstring(game:HttpGet("https://raw.githubusercontent.com/tempaccount42069/KitkatHub/refs/heads/main/KnifeSimulator.lua", true))()
 end,
 [17258520007] = function()
--- This will only load when the script is called for this game ID
 loadstring(game:HttpGet("https://raw.githubusercontent.com/tempaccount42069/KitkatHub/refs/heads/main/BelluPiece.lua", true))()
 end,
-[1122334455] = function()
--- Placeholder script for game 1122334455
-print("Script for Game 1122334455 loaded!")
--- Add your script content here for game ID 1122334455
+[2824139918] = function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/tempaccount42069/KitkatHub/refs/heads/main/MilitaryMadness.lua",true))()
 end,
 }
 
@@ -19,7 +15,7 @@ local currentGameID = game.PlaceId
 if gameScripts[currentGameID] then
 local scriptToLoad = gameScripts[currentGameID]
 local success, errorMsg = pcall(function()
-scriptToLoad() -- Call the function, which will execute the correct script
+scriptToLoad()
 end)
 
 if not success then
